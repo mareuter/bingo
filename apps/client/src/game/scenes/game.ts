b2ef3d40ea90abc9f9b83c5b337e9d35289addee
Michael Reuter <mareuternh@gmail.com>
2026-01-18 07:46:30 -0700

    Adding start new game button.

--- apps/client/src/game/scenes/game.ts
+++ apps/client/src/game/scenes/game.ts
@@ -8,6 +8,8 @@ import { NoMoreBingoBallsError } from '@repo/core/src/bingo-errors'
 import CardPanel from '../game-objects/card-panel'
 import BingoCard from '@repo/core/src/bingo-card'
 import { BALL_PANEL_BACKGROUND, hexNumToString } from '../common'
+import StartGameButton from '../game-objects/start-new-game-button'
+import BingoBall from '@repo/core/src/bingo-ball'
 
 const sleep = (delay: number) => new Promise((resolve) => setTimeout(resolve, delay))
 
@@ -18,6 +20,7 @@ class Game extends Scene {
   cardPanel: CardPanel
   timeline: Time.Timeline
   messagePanel: GameObjects.DOMElement
+  startNewGameButton: StartGameButton
 
   constructor() {
     super('Game')
@@ -48,29 +51,34 @@ class Game extends Scene {
       'padding-top': '5px',
     }
     this.messagePanel = this.add.dom(512, 70, 'div', style, 'Welcome!')
+    this.startNewGameButton = new StartGameButton(this, 512, 320)
+    this.events.on('startNewGame', this.startNewGame, this)
 
     this.cardPanel = new CardPanel(this, 512, 450, new BingoCard())
-
     this.events.on('haveWinningCard', this.handleWinningCard, this)
 
     this.timeline = this.add.timeline({
       from: 5000,
       run: () => this.announceBall(),
     })
+  }
 
+  async startNewGame() {
+    this.currentBallPanel.clear()
+    this.messagePanel.setText('Starting Game!')
     this.timeline.repeat().play()
+    await sleep(2000)
+    this.messagePanel.setText('')
   }
 
   announceBall() {
-    try {
-      const bb = this.gameLeader.announceBall()
-      this.currentBallPanel.updateBall(bb)
+    const bb = this.gameLeader.announceBall()
+    this.currentBallPanel.updateBall(bb)
+    if (bb.number !== BingoBall.GAME_OVER) {
       this.ballStatusPanel.updateDisplay(bb)
-    } catch (error) {
-      if (error instanceof NoMoreBingoBallsError) {
-        this.timeline.stop()
-        this.currentBallPanel.clear()
-      }
+    } else {
+      this.timeline.stop()
+      this.startNewGameButton.enable()
     }
   }
 
@@ -82,6 +90,7 @@ class Game extends Scene {
       this.currentBallPanel.clear()
       await sleep(2000)
       this.messagePanel.setText('Game Over!')
+      this.startNewGameButton.enable()
     } else {
       this.messagePanel.setText('Player cried Wolf!! ... Be careful!!')
       await sleep(2000)
